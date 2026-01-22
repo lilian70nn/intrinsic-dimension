252458fcec2fe35a2cb8e10489e9ea18badde2f7
Lily Li <lily@Lians-Macbook.local>
2026-01-22 23:34:36 +0100

    Add/update task experiments

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,6 +2,7 @@ import random
 from functools import partial
 import argparse
 import os
+import nltk
 
 import torch
 import numpy as np
@@ -22,7 +23,8 @@ from src.depths import getDepths
 from src.id.compute import compute_id_dynamics_across_models
 from src.plots import title_to_filename
 
-
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 
 
